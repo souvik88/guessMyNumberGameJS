4dffa8cec85bb3be07c9f5159dcc066dd2a9c798
Souvik Kundu <kivtech88@gmail.com>
2020-11-14 19:43:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,19 +11,24 @@ console.log(document.querySelector('.guess').value);
 */
 
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
 document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function() {
     const guess = Number(document.querySelector('.guess').value);
     console.log(guess, typeof guess);
 
-    if(!guess) {
+    if (!guess) {
         document.querySelector('.message').textContent = 'No Number!';
-    } else if(guess === secretNumber) {
+    } else if (guess === secretNumber) {
         document.querySelector('.message').textContent = 'Correct Number!';
     } else if (guess > secretNumber) {
         document.querySelector('.message').textContent = 'Too High';
+        score--;
+        document.querySelector('.score').textContent = score;
     } else if(guess < secretNumber) {
         document.querySelector('.message').textContent = 'Too Low';
+        score--;
+        document.querySelector('.score').textContent = score;
     }
 });
