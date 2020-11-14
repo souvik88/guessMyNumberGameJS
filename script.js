e76ff5008cddfdcee46a187b911e475c3b9e9a93
Souvik Kundu <kivtech88@gmail.com>
2020-11-15 01:02:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -14,6 +14,16 @@ const secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 document.querySelector('.number').textContent = secretNumber;
 
+function checkScore() {
+    if (score > 0) {
+        document.querySelector('.message').textContent = '📈 Too High!';
+        score--;
+        document.querySelector('.score').textContent = score;
+    } else {
+        document.querySelector('.message').textContent = '👎 You Lost!';
+    }
+}
+
 document.querySelector('.check').addEventListener('click', function () {
     const guess = Number(document.querySelector('.guess').value);
     console.log(guess, typeof guess);
@@ -23,20 +33,8 @@ document.querySelector('.check').addEventListener('click', function () {
     } else if (guess === secretNumber) {
         document.querySelector('.message').textContent = '✔ Correct Number';
     } else if (guess > secretNumber) {
-        if (score > 0) {
-            document.querySelector('.message').textContent = '📈 Too High!';
-            score--;
-            document.querySelector('.score').textContent = score;
-        } else {
-            document.querySelector('.message').textContent = '👎 You Lost!';
-        }
+        checkScore();
     } else if (guess < secretNumber) {
-        if (score > 0) {
-            document.querySelector('.message').textContent = '📉 Too Low!';
-            score--;
-            document.querySelector('.score').textContent = score;
-        } else {
-            document.querySelector('.message').textContent = '👎 You Lost!';
-        }
+        checkScore();
     }
 });
