98fa9a93e644cb2ec20e3bfbcc5801b9a5bdc5a6
Souvik Kundu <kivtech88@gmail.com>
2020-11-15 10:32:35 +0530

    commit

--- script.js
+++ script.js
@@ -9,34 +9,3 @@ document.querySelector('.score').textContent = 40;
 document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
 */
-
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
-document.querySelector('.number').textContent = secretNumber;
-let score = 20;
-
-document.querySelector('.check').addEventListener('click', function () {
-    const guess = Number(document.querySelector('.guess').value);
-    console.log(guess, typeof guess);
-
-    if(!guess) {
-        document.querySelector('.message').textContent = '⛔No Number!';
-    } else if (guess === secretNumber) {
-        document.querySelector('.message').textContent = '✔Correct Number';
-    } else if (guess > secretNumber) {
-        if (score > 0) {
-            document.querySelector('.message').textContent = '📈Too High!';
-            score--;
-            document.querySelector('.score').textContent = score;
-        } else {
-            document.querySelector('.message').textContent = '👎You Lost!';
-        }
-    } else if (guess < secretNumber) {
-        if (score > 0) {
-            document.querySelector('.message').textContent = '📉Too Low!';
-            score--;
-            document.querySelector('.score').textContent = score;
-        } else {
-            document.querySelector('.message').textContent = '👎You Lost!';
-        }
-    }
-});
